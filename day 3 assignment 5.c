08afa331125904a0c6b4003eeb7059c9b201429f
CSA0270 <130425697+Mahipal-192210096@users.noreply.github.com>
2023-04-16 20:41:10 +0530

    Create day 3 assignment 5.c

---
+++ day 3 assignment 5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int main() {
+    char str[MAX_SIZE];
+    printf("Enter a string: ");
+    fgets(str, MAX_SIZE, stdin);
+    str[strcspn(str, "\n")] = '\0'; // remove trailing newline from fgets
+
+    int freq[256] = {0}; // to store frequency of each character (ASCII characters)
+
+    // Count frequency of each character in the string
+    for (int i = 0; i < strlen(str); i++) {
+        freq[str[i]]++;
+    }
+
+    char maxChar = ' ';
+    int maxFreq = 0;
+
+    // Find the character with highest frequency
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > maxFreq) {
+            maxFreq = freq[i];
+            maxChar = (char)i;
+        }
+    }
+
+    printf("Max repeated character in the string: %c\n", maxChar);
+    printf("It occurs %d times\n", maxFreq);
+
+    return 0;
+}
