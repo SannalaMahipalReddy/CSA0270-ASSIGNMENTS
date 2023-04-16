3902a4a32bb8109b55c9f59edc5bc2f39176d112
CSA0270 <130425697+Mahipal-192210096@users.noreply.github.com>
2023-04-16 20:45:18 +0530

    Update day 3 assignment 4.c

--- day 3 assignment 4.c
+++ day 3 assignment 4.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-// Function to find the GCD of two numbers
 int gcd(int a, int b) {
     if (b == 0) {
         return a;
     }
     return gcd(b, a % b);
 }
-
-// Function to find the LCM of two numbers
 int lcm(int a, int b) {
     return (a * b) / gcd(a, b);
 }
