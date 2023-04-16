e80ea84c37ded57654cf84b2b342b55b94bff13e
CSA0270 <130425697+Mahipal-192210096@users.noreply.github.com>
2023-04-16 20:44:38 +0530

    Update day 3 assignment 5.c

--- day 3 assignment 5.c
+++ day 3 assignment 5.c
@@ -7,19 +7,15 @@ int main() {
     char str[MAX_SIZE];
     printf("Enter a string: ");
     fgets(str, MAX_SIZE, stdin);
-    str[strcspn(str, "\n")] = '\0'; // remove trailing newline from fgets
+    str[strcspn(str, "\n")] = '\0'; 
 
-    int freq[256] = {0}; // to store frequency of each character (ASCII characters)
-
-    // Count frequency of each character in the string
+    int freq[256] = {0}; 
     for (int i = 0; i < strlen(str); i++) {
         freq[str[i]]++;
     }
 
     char maxChar = ' ';
     int maxFreq = 0;
-
-    // Find the character with highest frequency
     for (int i = 0; i < 256; i++) {
         if (freq[i] > maxFreq) {
             maxFreq = freq[i];
