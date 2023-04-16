fd763cd57120c1b6630284dd2ce6c25a6c1ed2c7
CSA0270 <130425697+Mahipal-192210096@users.noreply.github.com>
2023-04-16 20:43:43 +0530

    Update day 3 assignment 6.c

--- day 3 assignment 6.c
+++ day 3 assignment 6.c
@@ -3,26 +3,19 @@
 #include <string.h>
 
 bool areAnagrams(char str1[], char str2[]) {
-    // Get the lengths of the strings
+   
     int len1 = strlen(str1);
     int len2 = strlen(str2);
-
-    // If lengths are not equal, they are not anagrams
     if (len1 != len2) {
         return false;
     }
 
-    // Arrays to store character counts
     int count1[26] = {0};
     int count2[26] = {0};
-
-    // Loop through both strings and count the occurrences of each character
     for (int i = 0; i < len1; i++) {
         count1[str1[i] - 'a']++;
         count2[str2[i] - 'a']++;
     }
-
-    // Compare the character counts
     for (int i = 0; i < 26; i++) {
         if (count1[i] != count2[i]) {
             return false;
